e20222ba55923a2680d93bab6044a58e564655c9
Sajal Tiwari <sajal.tiwari05@gmail.com>
2022-06-26 05:00:36 +0530

    26/6

---
+++ COURSE/Edureka/pytest/file1.py
@@ -0,0 +1,11 @@
+from re import X
+import pytest
+
+class Test:
+    def testone(self):
+        x='sajal'
+        assert 'a'in X
+
+    def testtwo(self):
+        x='k'
+        assert hasattr(x,'check') 
